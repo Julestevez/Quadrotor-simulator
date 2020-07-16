b2c6e4a3900b9f60a9401a5faa90934bc582c22c
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2020-07-16 19:23:50 +0200

    Correct code of a leader and follower robot

--- Horizontal control of a multidrone system/main.py
+++ Horizontal control of a multidrone system/main.py
@@ -33,18 +33,19 @@ dt=0.1
 Kxd1, Kxp1 = 0.76, 0.22
 Kyd1, Kyp1 = 0.76, 0.22
 Kxd2, Kxp2 = 0.76, 0.22
+Kyd2, Kyp2 = 0.76, 0.22
 
 #x_pos_desired = np.linspace(0,200,N)
-final_x_coordinate=150 
+final_x_coordinate=150
+final_y_coordinate=80 
 x_pos_desired = [final_x_coordinate]*N
 x_pos_desired[0:150] = np.linspace(0,final_x_coordinate,150)
 #x_pos_desired = np.linspace(0,final_x_coordinate,N)
-
-y_pos_desired = [0]*N
 #x_vel_desired = (x_pos_desired - x_pos)/(10*dt)
 #x_accel_desired = (x_pos_desired-x_pos)/(100*dt*dt)
 
-#y_pos_desired = 0
+y_pos_desired = [final_y_coordinate]*N
+y_pos_desired[0:150] = np.linspace(0,final_y_coordinate,150)
 #y_vel_desired = (y_pos_desired - y)/(10*dt)
 #y_accel_desired = (y_pos_desired-y)/(100*dt*dt)
 
@@ -53,9 +54,12 @@ y_pos_desired = [0]*N
                             #theta_dot phi_dot gamma_dot 
                             # x         y        z 
                             # x_dot     y_dot   z_dot]
-States=  [0]*12
-States2= [0]*12
+
+States=  [0]*12 #States of the Quaddrotor1
+States2= [0]*12 #States of the Quadrotor2
+
 States2[6]=-50 #starting position
+States2[7]=-60
 #Desired states representation: [x_pos_d, x_vel_d, x_accel_d, 
                                 # y_pos_d, y_vel_d, y_accel_d, 
                                 # theta_d, phi_d, psi_d, height_desired]
@@ -72,25 +76,28 @@ for j in range(N):
     x_accel_desired = (x_vel_desired - States[9])/(100*dt*dt)
     x_accel_desired = 0
     y_vel_desired = (y_pos_desired[j] - States[7])/(50*dt)
-    y_accel_desired = (y_vel_desired - States[10])/(100*dt*dt)
+    y_accel_desired = 0
 
 
-
+    #Assign desired position, speed and velocity in X axis
     S_desired[0], S_desired[1], S_desired[2] = x_pos_desired[j],x_vel_desired,x_accel_desired
+
+    #Assign desired position, speed and velocity in Y axis
+    S_desired[3], S_desired[4], S_desired[5] = y_pos_desired[j],y_vel_desired,y_accel_desired
     
-    #ANGLE OBJECTIVE
+    #OBJECTIVE ANGLES
     #Theta
     S_desired[6] = angle_objective(S_desired[0:3],States[6],States[9],States[0],Thrust,Kxp1,Kxd1)
     
     #Phi
-    #S_desired[7] = angle_objective(S_desired[3:7],States[7],States[10],States[1],Thrust[j-1],Kyp1,Kyd1)
+    S_desired[7] = angle_objective(S_desired[3:7],States[7],States[10],States[1],Thrust,Kyp1,Kyd1)
     if (j>50 and abs(States[6]-final_x_coordinate)<5 and States[9]<0.5 and States[9]>-0.5):
         S_desired[6]=0
         break
     
     #DRONE height position, velocity and acceleration
     #OBTAINING OF THE STATES OF THE QUADROTOR
-    States[0:6], Thrust_calc, z_accel, x_accel= quadrotor(States,S_desired[6:11],Thrust)
+    States[0:6], Thrust_calc, z_accel, x_accel, y_accel= quadrotor(States,S_desired[6:11],Thrust)
     
     
     if Thrust_calc-Thrust>0.05:
@@ -101,10 +108,12 @@ for j in range(N):
 
     #OBTAINTION OF FUZZY Kp-Kd
     Kxp1,Kxd1 = OuterLoopAdaptiveController(S_desired[6],States[0],Kxp1,Kxd1) #X Address
-    #Kyp,Kyd = (S_desired[6],States[1],Kyp,Kyd) #Y Address
+    Kyp1,Kyd1 = OuterLoopAdaptiveController(S_desired[7],States[1],Kyp1,Kyd1) #Y Address
 
-    #OBTAINTION OF X_POS AND X_VEL
-    States[9],States[6]= EulerIntegration(x_accel,States[9],States[6])
+    #OBTAINTION OF POS AND VELOCITY
+    States[9],States[6]= EulerIntegration(x_accel,States[9],States[6]) #X axis
+
+    States[10],States[7]= EulerIntegration(y_accel,States[10],States[7]) #y axis
     
 
     
@@ -112,25 +121,31 @@ for j in range(N):
     ###################################################################
     ###################### Follower DRONE #############################
     ###################################################################
-    x_pos_desired2, y_pos_desired2 = Follower(States2[5:7],Old_States)
+    x_pos_desired2, y_pos_desired2 = Follower(States[6:8],Old_States) #previous drone states as argument
     x_vel_desired2 = (x_pos_desired2 - States2[6])/(50*dt)
     x_accel_desired2 = 0
     y_vel_desired2 = (y_pos_desired2 - States2[7])/(50*dt)
-    y_accel_desired = 0
+    y_accel_desired2 = 0
 
+    #Assign desired position, speed and velocity in X axis
     S_desired2[0], S_desired2[1], S_desired2[2] = x_pos_desired2,x_vel_desired2,x_accel_desired2
+
+    #Assign desired position, speed and velocity in Y axis
+    S_desired2[3], S_desired2[4], S_desired2[5] = y_pos_desired2,y_vel_desired2,y_accel_desired2
     
-    #ANGLE OBJECTIVE
-    #Theta
+    #ANGLE OBJECTIVE 
+    #Theta - X direction
     S_desired2[6] = angle_objective(S_desired2[0:3],States2[6],States2[9],States2[0],Thrust2,Kxp2,Kxd2)
     
-    #Phi
-    #S_desired[7] = angle_objective(S_desired[3:7],States[7],States[10],States[1],Thrust[j-1],Kyp1,Kyd1)
-  
+    #Phi - Y direction
+    S_desired2[7] = angle_objective(S_desired2[3:7],States2[7],States2[10],States[1],Thrust2,Kyp2,Kyd2)
+    if (j>50 and abs(States2[6]-S_desired2[3])<5 and States2[10]<0.5 and States[10]>-0.5):
+        S_desired2[7]=0
+        break
     
     #DRONE height position, velocity and acceleration
     #OBTAINING OF THE STATES OF THE QUADROTOR
-    States2[0:6], Thrust_calc2, z_accel2, x_accel2= quadrotor(States2,S_desired2[6:11],Thrust2)
+    States2[0:6], Thrust_calc2, z_accel2, x_accel2, y_accel2= quadrotor(States2,S_desired2[6:11],Thrust2)
     
     
     if Thrust_calc2-Thrust2>0.05:
@@ -141,19 +156,23 @@ for j in range(N):
 
     #OBTAINTION OF FUZZY Kp-Kd
     Kxp2,Kxd2 = OuterLoopAdaptiveController(S_desired2[6],States2[0],Kxp2,Kxd2) #X Address
-    #Kyp,Kyd = (S_desired[6],States[1],Kyp,Kyd) #Y Address
+    Kyp2,Kyd2 = OuterLoopAdaptiveController(S_desired2[7],States2[1],Kyp2,Kyd2) #Y Address
 
-    #OBTAINTION OF X_POS AND X_VEL
-    States2[9],States2[6]= EulerIntegration(x_accel2,States2[9],States2[6])
+    #OBTAINTION OF POS and VELOCITY
+    States2[9],States2[6]= EulerIntegration(x_accel2,States2[9],States2[6]) #x axis
+    States2[10],States2[7]= EulerIntegration(y_accel2,States2[10],States2[7]) #y axis
 
-
-
-    
+   
     ###################################################################
     ######################## VISUALIZATION ############################
     ###################################################################
-    plt.plot(j,x_pos_desired2,'ob')
-    #plt.plot(States2[6],'or')
+    
+    plt.plot(States[6],States[7],'ob')
+    plt.plot(States2[6],States2[7],'or')
+    #plt.plot(j,States2[6],'or')
+    #plt.plot(j,y_pos_desired2,'*g')
+    #plt.plot(j,y_pos_desired2,'or')
+    #plt.plot(j,States2[6],'or')
     plt.pause(0.1)
 
     #plt.plot(j,States[9],'or') #graphic of x_pos vs time [j = =0.1s]
