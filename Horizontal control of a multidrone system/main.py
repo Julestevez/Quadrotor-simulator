7f84afc57255527324ebc589639db690e84100fa
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2020-07-17 11:22:42 +0200

    Added code for visualization in 3D

--- Horizontal control of a multidrone system/main.py
+++ Horizontal control of a multidrone system/main.py
@@ -12,6 +12,7 @@ from Quadrotor import angle_objective
 from Quadrotor import OuterLoopAdaptiveController
 from Quadrotor import EulerIntegration
 from Quadrotor import Follower
+from Visual3D import Visual3D
 
 
 #***Dynamic parameters of DRONE****
@@ -66,8 +67,8 @@ States2[7]=-60
 S_desired = [0]*10
 S_desired2= [0]*10
 
-
-
+fig = plt.figure()
+ax = fig.gca(projection='3d') 
 for j in range(N):
             
     Old_States=States[5:7] #temporary function
@@ -166,9 +167,35 @@ for j in range(N):
     ###################################################################
     ######################## VISUALIZATION ############################
     ###################################################################
+    VecStart_x,VecStart_y,VecStart_z, VecEnd_x,VecEnd_y,VecEnd_z= Visual3D(States[0],States[1],States[6],States[7],States2[0],States2[1],States2[6],States2[7])
+  
+
+    ax.cla()
+    ax.set_xlim3d(-100, 200)
+    ax.set_ylim3d(-100,100)
+    ax.set_zlim3d(0,100)
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    for i in range(4):
+        ax.plot([VecStart_x[i], VecEnd_x[i]], [VecStart_y[i],VecEnd_y[i]],zs=[VecStart_z[i],VecEnd_z[i]])
     
-    plt.plot(States[6],States[7],'ob')
-    plt.plot(States2[6],States2[7],'or')
+    ax.scatter3D(States[6],States[7],50,s=30)
+    ax.scatter3D(States2[6],States2[7],50,s=30)
+    #ax.scatter3D(States3[6],States3[7],0)
+    #ax.scatter3D(vectorX1,0,z1)
+   
+
+    #plt.plot(j,States[6],'ob')          #plot of leader drone
+    #plt.plot(j,x_pos_desired2,'ob')    #plot of desired position of follower drone
+    #plt.plot(j,States2[6],'or')        #plot of follower drone
+
+    #plt.gca()
+
+    #plt.plot(j,States[9],'or') #graphic of x_pos vs time [j = =0.1s]
+
+    #plt.plot(States[6],States[7],'ob')
+    #plt.plot(States2[6],States2[7],'or')
     #plt.plot(j,States2[6],'or')
     #plt.plot(j,y_pos_desired2,'*g')
     #plt.plot(j,y_pos_desired2,'or')
