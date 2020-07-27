a3abc3f77ea9b1388b790b4c67c674b6cd5c59f1
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2020-07-27 15:33:53 +0200

    Made the code more scalable

--- Horizontal control of a multidrone system/Visual3D.py
+++ Horizontal control of a multidrone system/Visual3D.py
@@ -5,49 +5,32 @@ import math
 
 
 
-def Visual3D(theta_leader, phi_leader, x_leader, y_leader, theta_follower, phi_follower, x_follower, y_follower):
+def Visual3D(theta, phi, x_pos, y_pos):
       
     #plot of the UAV
-    #DRONE1
-    X1=x_leader-20*math.cos(theta_leader)
-    X2=x_leader
-    X3=x_follower-20*math.cos(theta_follower)
-    X4=x_follower
-
-    Y1=y_leader
-    Y2=y_leader-20*math.cos(phi_leader)
-    Y3=y_follower
-    Y4=y_follower-20*math.cos(phi_follower)
-
-    Z1=50+20*math.sin(theta_leader)
-    Z2=50+20*math.sin(phi_leader)
-    Z3=50+20*math.sin(theta_follower)
-    Z4=50+20*math.sin(phi_follower)
-
+    X1=x_pos-20*math.cos(theta)
+    X2=x_pos
+    X3=x_pos+20*math.cos(theta)
+    X4=x_pos
+    
+    Y1=y_pos
+    Y2=y_pos-20*math.cos(phi)
+    Y3=y_pos
+    Y4=y_pos+20*math.cos(phi)
     
 
-    #DRONE2
-    X5=x_leader+20*math.cos(theta_leader)
-    X6=x_leader
-    X7=x_follower+20*math.cos(theta_follower)
-    X8=x_follower
-
-    Y5=y_leader
-    Y6=y_leader+20*math.cos(phi_leader)
-    Y7=y_follower
-    Y8=y_follower+20*math.cos(phi_follower)
-
-    Z5=50-20*math.sin(theta_leader)
-    Z6=50-20*math.sin(phi_leader)
-    Z7=50-20*math.sin(theta_follower)
-    Z8=50-20*math.sin(phi_follower)
+    Z1=50+20*math.sin(theta)
+    Z2=50+20*math.sin(phi)
+    Z3=50-20*math.sin(theta)
+    Z4=50-20*math.sin(phi)
     
-    VecStart_x=[X1, X2, X3, X4]
-    VecStart_y=[Y1, Y2, Y3, Y4]
-    VecStart_z=[Z1, Z2, Z3, Z4]
-    VecEnd_x= [X5, X6, X7, X8]
-    VecEnd_y= [Y5, Y6, Y7, Y8]
-    VecEnd_z= [Z5, Z6, Z7, Z8]
+      
+    VecStart_x=[X1, X2]
+    VecStart_y=[Y1, Y2]
+    VecStart_z=[Z1, Z2]
+    VecEnd_x= [X3, X4]
+    VecEnd_y= [Y3, Y4]
+    VecEnd_z= [Z3, Z4]
 
     return VecStart_x,VecStart_y,VecStart_z, VecEnd_x,VecEnd_y,VecEnd_z
     
