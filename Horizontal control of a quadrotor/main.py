607b166987104919d4b6baa32399212154af3a5a
Julián Estévez <52918745+Julestevez@users.noreply.github.com>
2020-09-13 17:58:47 +0200

    Updated the code (ReadMe work)

--- Horizontal control of a quadrotor/main.py
+++ Horizontal control of a quadrotor/main.py
@@ -14,17 +14,17 @@ from Quadrotor import EulerIntegration
 
 
 #***Dynamic parameters of DRONE****
-L=25 
+L=0.25 # [m]
 b = 1e-5 
-I = np.diag([5e-7, 5e-7, 10e-7]) 
+I = np.diag([5e-3, 5e-3, 10e-3])  # [kgm2]
 k=3e-5
-m=0.5
+m=0.5 #[kg]
 kd=0.25
-g=9.80
+g=9.80 #m/s2
 
 
 #inicialization of variables
-N=300
+N=400 #time steps
 Thrust=5
 dt=0.1
 
@@ -32,12 +32,8 @@ Kxd1, Kxp1 = 0.76, 0.22
 Kyd1, Kyp1 = 0.76, 0.22
 
 #x_pos_desired = np.linspace(0,200,N)
-final_x_coordinate=150 
-x_pos_desired = [final_x_coordinate]*N
-x_pos_desired[0:150] = np.linspace(0,final_x_coordinate,150)
-#x_pos_desired = np.linspace(0,final_x_coordinate,N)
-
-y_pos_desired = [0]*N
+final_x_coordinate=50 
+final_y_coordinate=50
 
 
 #State space representation: [theta     phi     gamma 
@@ -55,52 +51,68 @@ S_desired = [0]*10
 
 for j in range(N):
             
-    x_vel_desired = (x_pos_desired[j] - States[6])/(50*dt)
-    #x_accel_desired = (x_vel_desired - States[9])/(100*dt*dt)
+    x_vel_desired = (final_x_coordinate - States[6])/(50*dt)
     x_accel_desired = 0
-    y_vel_desired = (y_pos_desired[j] - States[7])/(50*dt)
-    y_accel_desired = (y_vel_desired - States[10])/(100*dt*dt)
+    y_vel_desired = (final_y_coordinate - States[7])/(50*dt)
+    y_accel_desired = 0
 
 
-
-    S_desired[0], S_desired[1], S_desired[2] = x_pos_desired[j],x_vel_desired,x_accel_desired
+    S_desired[0], S_desired[1], S_desired[2] = final_x_coordinate,x_vel_desired,x_accel_desired
+    S_desired[3], S_desired[4], S_desired[5] = final_y_coordinate,y_vel_desired,y_accel_desired
     
-    #ANGLE OBJECTIVE
+    #DESIRED ANGLE
     #Theta
     S_desired[6] = angle_objective(S_desired[0:3],States[6],States[9],States[0],Thrust,Kxp1,Kxd1)
     
     #Phi
-    #S_desired[7] = angle_objective(S_desired[3:7],States[7],States[10],States[1],Thrust[j-1],Kyp1,Kyd1)
-    if (j>50 and abs(States[6]-final_x_coordinate)<5 and States[9]<0.5 and States[9]>-0.5):
-        S_desired[6]=0
-        break #finish the programm if it reaches to desired pos with approx 0 velocity
+    S_desired[7] = angle_objective(S_desired[3:7],States[7],States[10],States[1],Thrust,Kyp1,Kyd1)
+    
     
     #DRONE height position, velocity and acceleration
-    #CALCULUS OF THE STATES OF THE QUADROTOR
-    States[0:6], Thrust_calc, z_accel, x_accel= quadrotor(States,S_desired[6:11],Thrust)
+    #OBTAINING OF THE STATES OF THE QUADROTOR
+    States[0:6], Thrust_calc, z_accel, x_accel, y_accel= quadrotor(States,S_desired[6:11],Thrust)
     
+    #the increment of the thrust is smooth
     if Thrust_calc-Thrust>0.05:
         Thrust=Thrust_calc+0.05
     elif Thrust-Thrust_calc>0.05:
         Thrust=Thrust_calc-0.05
                 
 
-    #CALCULUS OF FUZZY Kp-Kd
-    Kxp1,Kxd1 = OuterLoopAdaptiveController(S_desired[6],States[0],Kxp1,Kxd1) #X Address
-    #Kyp,Kyd = (S_desired[6],States[1],Kyp,Kyd) #Y Address
-
-    #CALCULUS OF X_POS AND X_VEL
-    States[9],States[6]= EulerIntegration(x_accel,States[9],States[6])
+    #OBTAINTION OF X_POS AND X_VEL
+    States[9],States[6]  = EulerIntegration(x_accel,States[9],States[6])
+    States[10],States[7] = EulerIntegration(y_accel,States[10],States[7])
     
-    plt.subplot(3,1,1)
-    plt.plot(j,States[6],'or')
-    #plt.plot(j,x_vel_desired,'ob')
-    plt.subplot(3,1,3)
-    plt.plot(j,States[6],'og')
-    #plt.plot(j,x_accel_desired,'or')
-    plt.subplot(3,1,1)
-    plt.plot(j,x_accel,'or')
-    #plt.plot(j,x_pos_desired[j],'or')
+    #plot displacement
+    plt.subplot(3,2,1)
+    plt.title("Variables in X direction")
+    plt.plot(j,States[6],'og',markersize=2)
+    plt.ylabel("displacement [m]")
 
-    #plt.plot(j,States[9],'or') #graphic of x_pos vs time [j = =0.1s]
+    plt.subplot(3,2,2)
+    plt.title("Variables in Y direction")
+    plt.plot(j,States[7],'og',markersize=2)
+    plt.ylabel("displacement [m]")
+
+    #plot velocity    
+    plt.subplot(3,2,3)
+    plt.plot(j,States[9],'or',markersize=2)
+    plt.ylabel("speed [m/s]")
+
+    #plot velocity    
+    plt.subplot(3,2,4)
+    plt.plot(j,States[10],'or',markersize=2)
+    plt.ylabel("speed [m/s]")
+    
+    #plot acceleration
+    plt.subplot(3,2,5)
+    plt.ylabel("acceleration [m/s2]")
+    plt.xlabel("time [s/10]")
+    plt.plot(j,x_accel,'or',markersize=2)
+
+    plt.subplot(3,2,6)
+    plt.ylabel("acceleration [m/s2]")
+    plt.xlabel("time [s/10]")
+    plt.plot(j,y_accel,'or',markersize=2)
+        
 plt.show()
